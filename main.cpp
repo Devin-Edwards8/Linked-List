fec06bcdf8a96fc20470d0ce372f0de1700e734e
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2022-05-10 11:15:45 -0400

    Update main.cpp
    Updated to separate functions into multiple files.

--- main.cpp
+++ main.cpp
@@ -1,70 +1,7 @@
-#include <iostream>
-#include <string>
 #include <fstream>
-#include <cstdlib>
+#include "linked_list.h"
 
 using namespace std;
-
-struct Data {
-    string name;
-    float grades[12];
-    int ID;
-};
-
-typedef struct Node{
-    Data student;
-    Node *next;
-} Node;
-
-void insert_node(Node **list_head_ptr, Node *newNodePtr) {
-    newNodePtr->next = *list_head_ptr;
-    *list_head_ptr = newNodePtr;
-}
-
-void create_node(Node **list_head_ptr, string student_name, int student_ID, float grades[12]) {
-    Node *newNodePtr = (Node*) malloc(sizeof(Node));
-    newNodePtr->student.name = student_name;
-    newNodePtr->student.ID = student_ID;
-    for(int i = 0; i < 12; i++)
-        newNodePtr->student.grades[i] = grades[i];
-    
-    insert_node(list_head_ptr, newNodePtr);
-}
-
-void read_file(Node **list_head_ptr, string filename){
-    string line, student_name;
-    int student_ID = 0;
-    float grades[12];
-    for(int i = 0; i < 12; i++)
-        grades[i] = 0;
-    ifstream records;
-    records.open(filename, ios::in);
-    getline(records, line); // consume header
-    
-    while(!records.eof()) {
-        
-        getline(records, student_name);
-        getline(records, line);
-        student_ID = stoi(line);
-        
-        for(int i = 0; i < 12; i++) {
-            records >> line;
-            grades[i] = stof(line);
-        }
-        getline(records, line); //consume whitespace ?
-        
-        create_node(list_head_ptr, student_name, student_ID, grades);
-    }
-    records.close();
-}
-
-void print_list(Node **list_head) {
-    Node *traversePtr = *list_head;
-    while(traversePtr != NULL) {
-        cout << traversePtr->student.name << "\n";
-        traversePtr = traversePtr->next;
-    }
-}
 
 int main() {
     Node *list_head = NULL;
