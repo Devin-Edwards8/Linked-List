78082783e3c44451a761058140d54bfe06d5f8b4
Devin Edwards <98787106+siiiiw@users.noreply.github.com>
2022-05-10 14:01:44 -0400

    Update main.cpp
    updated to add choice of file inout or manual input

--- main.cpp
+++ main.cpp
@@ -1,21 +1,28 @@
 #include <fstream>
+#include <iostream>
 #include "linked_list.h"
 
 using namespace std;
 
 int main() {
     Node *list_head = NULL;
-    string line, cats[4];
-    ifstream records;
-    records.open("class_records", ios::in);
-    for(int i = 0; i < 4; i++) {
-        records >> line;
-        cats[i] = line;
+    string choice;
+
+    cout << "Would you like to enter a file with student info? (yes or no) : ";
+    cin >> choice;
+    if(choice == "yes") {
+        string line, cats[4], filename;
+        cout << "Enter filename : ";
+        cin >> filename;
+        ifstream records;
+        records.open(filename, ios::in);
+        records.close();
+        int count = read_file(&list_head, filename);
+        cout << "A list of " << count << " students has been created. \n";
+    } else {
+        cout << "Enter Student Info Manually. \n";
+        create_manual(&list_head);
     }
-    records.close();
-    read_file(&list_head, "class_records");
-    
-    print_list(&list_head);
     
     return 0;
 }
