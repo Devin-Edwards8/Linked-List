a46ae1b22e8a87561d76acd8302a6275579bef0b
DevinEdwards <98787106+siiiiw@users.noreply.github.com>
2022-05-10 11:16:21 -0400

    Create linked_list.h

---
+++ linked_list.h
@@ -0,0 +1,22 @@
+#include <string>
+
+using namespace std;
+
+struct Data {
+    string name;
+    float grades[12];
+    int ID;
+};
+
+typedef struct Node{
+    Data student;
+    Node *next;
+} Node;
+
+void insert_node(Node **list_head_ptr, Node *newNodePtr);
+
+void create_node(Node **list_head_ptr, string student_name, int student_ID, float grades[12]);
+
+void read_file(Node **list_head_ptr, string filename);
+
+void print_list(Node **list_head);
