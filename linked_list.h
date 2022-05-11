4a97a454dc37339eb9bdd57c0a8576197674f563
Devin Edwards <98787106+siiiiw@users.noreply.github.com>
2022-05-11 13:39:10 -0400

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -22,3 +22,18 @@ int read_file(Node **list_head_ptr, string filename);
 void print_list(Node **list_head);
 
 void create_manual(Node **list_head_ptr);
+
+void calculate_grade_by_ID(Node *list_head);
+
+void delete_student(Node *list_head);
+
+void update_grade(Node *list_head);
+
+void add_student(Node **list_head_ptr);
+
+void quit(Node **list_head_ptr, string cats[4]);
+
+float calc_avg(float grade1, float grade2, float grade3, bool ignore);
+
+float calc_total(float grades[12], float weights[4], bool ignore);
+
