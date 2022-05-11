66a1bbeaed87d721045a9aaeaf7684610ee0a8fb
Devin Edwards <98787106+siiiiw@users.noreply.github.com>
2022-05-11 13:39:58 -0400

    Update print_list.cpp
    updated to print everything

--- print_list.cpp
+++ print_list.cpp
@@ -1,10 +1,15 @@
 #include "linked_list.h"
 #include <iostream>
 
+/*This is a test function, to see if the list is created properly*/
 void print_list(Node **list_head) {
     Node *traversePtr = *list_head;
     while(traversePtr != NULL) {
         cout << traversePtr->student.name << "\n";
+        cout << traversePtr->student.ID << "\n";
+        for(int i = 0; i < 12; i++) {
+            cout << traversePtr->student.grades[i] << " ";
+        }
         traversePtr = traversePtr->next;
     }
 }
